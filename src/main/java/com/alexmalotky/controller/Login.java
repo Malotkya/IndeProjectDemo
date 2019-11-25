e02bee26d6825d0fb8c620f3eda843e7122d4227
Malotkya <ajmalotky@madisoncollege.edu>
2019-11-25 13:23:48 -0600

    Redirect to account page when logging in from index page

--- src/main/java/com/alexmalotky/controller/Login.java
+++ src/main/java/com/alexmalotky/controller/Login.java
@@ -2,6 +2,9 @@ package com.alexmalotky.controller;
 
 
 import com.alexmalotky.util.LoginServlet;
+import com.alexmalotky.util.PasswordManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,10 +16,21 @@ import java.io.IOException;
 @WebServlet( urlPatterns = {"/Login"} )
 public class Login extends LoginServlet {
 
+    private final Logger logger = LogManager.getLogger(Login.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         checkForLogin(request);
-        response.sendRedirect(request.getHeader("referer"));
-        //TODO: redirect to account instead of index
+        String redirect = request.getHeader("referer");
+
+        if( isIndexPage(redirect, request.getContextPath()) )
+            redirect += "Account";
+
+        response.sendRedirect(redirect);
+    }
+
+    private boolean isIndexPage(String targetUrl, String indexUrl) {
+        int index = targetUrl.lastIndexOf(indexUrl) + indexUrl.length() + 1;
+        return index >= targetUrl.length();
     }
 }
