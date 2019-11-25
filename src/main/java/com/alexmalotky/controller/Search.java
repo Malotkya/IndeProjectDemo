b07022e0fb6c57ea2933debde4c54fd31fa246ef
Malotkya <ajmalotky@madisoncollege.edu>
2019-11-25 13:14:04 -0600

    Built Search Functionality and Page

--- src/main/java/com/alexmalotky/controller/Search.java
+++ src/main/java/com/alexmalotky/controller/Search.java
@@ -1,22 +1,42 @@
 package com.alexmalotky.controller;
 
+import com.alexmalotky.entity.Recipe;
+import com.alexmalotky.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @WebServlet( urlPatterns = {"/Search"} )
 public class Search extends HttpServlet {
 
+    GenericDao<Recipe> dao = new GenericDao<>(Recipe.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String query = request.getParameter("q");
-        List<String> tokens = Arrays.asList(query.split("\\s"));
 
+        if(query != null) {
+            String[] tokens = query.split("\\s");
+            Map<String, String> params = new HashMap<>();
 
+            for(String token : tokens) {
+                if( !token.equals("") )
+                    params.put("name", "%" + token + "%");
+            }
+
+            List<Recipe> results = dao.findByPropertyLike(params);
+
+            request.setAttribute("results", results);
+            request.setAttribute("query", query);
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/search.jsp");
+        dispatcher.forward(request, response);
     }
 }
--- src/main/webapp/search.jsp
+++ src/main/webapp/search.jsp
@@ -1,12 +1,25 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@include file="jsp/header.jsp"%>
 <%@include file="jsp/topbar.jsp"%>
-<c:choose>
-    <c:when test="${empty results}">
-        <%@include file="jsp/searchBar.jsp"%>
-    </c:when>
-    <c:otherwise>
-        <!-- Display Results Here -->
-    </c:otherwise>
-</c:choose>
+<%@include file="jsp/searchBar.jsp"%>
+
+<c:set var="thisPage" value="Search" scope="request"/>
+
+<c:if test="${results != null}">
+    <ul>
+    <c:choose>
+        <c:when test="${empty results}">
+            <li>
+                <strong>There are no results to display</strong>
+            </li>
+        </c:when>
+        <c:otherwise>
+            <c:forEach items="${results}" var="recipe">
+                <!-- TODO: add a favorites button -->
+                <%@include file="jsp/listItemRecipe.jsp"%>
+            </c:forEach>
+        </c:otherwise>
+    </c:choose>
+    </ul>
+</c:if>
 <%@include file="jsp/footer.jsp"%>
