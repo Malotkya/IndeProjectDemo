3d377f836044e3d387045060daa198c7ca7394da
Malotkya <ajmalotky@madisoncollege.edu>
2019-12-16 14:12:53 -0600

    Added validation

--- src/main/webapp/js/newUser.js
+++ src/main/webapp/js/newUser.js
@@ -1,3 +1,116 @@
 const init = () => {
-    
-}; window.onLoad = init;
+    document.querySelector("#newUser").addEventListener("submit", validate);
+
+    document.querySelector("#username").addEventListener("change", checkUserName);
+    document.querySelector("#password1").addEventListener("change", checkPassword);
+    document.querySelector("#password2").addEventListener("change", comparePassword);
+    document.querySelector("#firstName").addEventListener("change", checkFirstName);
+    document.querySelector("#lastName").addEventListener("change", checkLastName);
+    document.querySelector("#email").addEventListener("change", checkEmail);
+
+    document.querySelector("#join").addEventListener("click", submitForm);
+
+}; window.onload = init;
+
+const submitForm = () => {
+    document.querySelector("#newUser").submit();
+};
+
+const validate = event => {
+    let invalidTest = event.currentTarget.querySelectorAll(".is-invalid").length;
+
+    return invalidTest === 0;
+};
+
+const checkUserName = async(event) => {
+    let input = event.currentTarget.parentNode;
+    let username = input.querySelector("#username").value;
+
+
+    let response = await fetch(`../GroupProject/thinkingOfSkywalker/uniqueValues/users/user_name/${username}`);
+
+    if(response.ok) {
+        let text = await response.text();
+
+        if (text === "true") {
+            input.querySelector("input").setAttribute("class", "form-control is-invalid");
+            input.querySelector("div").innerText = "Username is already taken";
+        } else {
+            input.querySelector("input").setAttribute("class", "form-control is-valid");
+            input.querySelector("div").innerText = "";
+        }
+    }
+};
+
+const checkPassword = event => {
+    let input = event.currentTarget.parentNode;
+    let password = input.querySelector("#password1").value;
+
+    if(password === "") {
+        input.querySelector("input").setAttribute("class", "form-control is-invalid");
+        input.querySelector("div").innerText = "Password can't be empty";
+    } else {
+        input.querySelector("input").setAttribute("class", "form-control is-valid");
+        input.querySelector("div").innerText = "";
+    }
+};
+
+const comparePassword = event => {
+    let input = event.currentTarget.parentNode;
+    let password1 = document.querySelector("#password1").value;
+    let password2 = document.querySelector("#password2").value;
+
+    if(password1 !== password2) {
+        input.querySelector("input").setAttribute("class", "form-control is-invalid");
+        input.querySelector("div").innerText = "Passwords must match";
+    } else {
+        input.querySelector("input").setAttribute("class", "form-control is-valid");
+        input.querySelector("div").innerText = "";
+    }
+};
+
+const checkFirstName = event => {
+    let input = event.currentTarget.parentNode;
+    let password = input.querySelector("#firstName").value;
+
+    if(password === "") {
+        input.querySelector("input").setAttribute("class", "form-control is-invalid");
+        input.querySelector("div").innerText = "First Name can't be empty";
+    } else {
+        input.querySelector("input").setAttribute("class", "form-control is-valid");
+        input.querySelector("div").innerText = "";
+    }
+};
+
+const checkLastName = event => {
+    let input = event.currentTarget.parentNode;
+    let password = input.querySelector("#lastName").value;
+
+    if(password === "") {
+        input.querySelector("input").setAttribute("class", "form-control is-invalid");
+        input.querySelector("div").innerText = "Last Name can't be empty";
+    } else {
+        input.querySelector("input").setAttribute("class", "form-control is-valid");
+        input.querySelector("div").innerText = "";
+    }
+};
+
+const checkEmail = async(event) => {
+    let input = event.currentTarget.parentNode;
+    let email = input.querySelector("#email").value;
+
+
+    let response = await fetch(`../GroupProject/thinkingOfSkywalker/validateEmails/${email}`);
+
+    if(response.ok) {
+        let text = await response.text();
+
+        if (text === "false") {
+            input.querySelector("input").setAttribute("class", "form-control is-invalid");
+            input.querySelector("div").innerText = "Username is already taken";
+        } else {
+            input.querySelector("input").setAttribute("class", "form-control is-valid");
+            input.querySelector("div").innerText = "";
+        }
+    }
+};
--- src/main/webapp/newUser.jsp
+++ src/main/webapp/newUser.jsp
@@ -10,8 +10,7 @@
 <%@include file="jsp/header.jsp"%>
 <%@include file="jsp/topbar.jsp"%>
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<form class="form" action="NewUser" method="POST">
-    <!-- TODO: add validation -->
+<form class="form" action="NewUser" method="POST" id="newUser">
     <div class="modal-dialog">
         <div class="modal-content">
 
@@ -20,32 +19,51 @@
             </div>
 
             <div class="modal-body">
+
                 <label for="username">Username:</label>
-                <input type="text" name="username" id="username" class="form-control"/>
+                <div class="input-group">
+                    <input type="text" name="username" id="username" class="form-control"/>
+                    <div class="invalid-feedback"></div>
+                </div>
 
                 <label for="password1">Enter Password:</label>
-                <input type="password" name="password1" id="password1" class="form-control"/>
+                <div class="input-group">
+                    <input type="password" name="password1" id="password1" class="form-control"/>
+                    <div class="invalid-feedback"></div>
+                </div>
 
                 <label for="password2">Re-Enter Password:</label>
-                <input type="password" name="password2" id="password2" class="form-control"/>
+                <div class="input-group">
+                    <input type="password" name="password2" id="password2" class="form-control"/>
+                    <div class="invalid-feedback"></div>
+                </div>
 
                 <label for="firstName">First Name:</label>
-                <input type="text" name="firstName" id="firstName" class="form-control"/>
+                <div class="input-group">
+                    <input type="text" name="firstName" id="firstName" class="form-control"/>
+                    <div class="invalid-feedback"></div>
+                </div>
 
                 <label for="lastName">Last Name:</label>
-                <input type="text" name="lastName" id="lastName" class="form-control"/>
+                <div class="input-group">
+                    <input type="text" name="lastName" id="lastName" class="form-control"/>
+                    <div class="invalid-feedback"></div>
+                </div>
 
                 <label for="email">Email:</label>
-                <input type="email" name="email" id="email" class="form-control"/>
+                <div class="input-group">
+                    <input type="email" name="email" id="email" class="form-control"/>
+                    <div class="invalid-feedback"></div>
+                </div>
             </div>
 
             <div class="modal-footer flex-column">
                 <div class="g-recaptcha" data-sitekey="6LeP574UAAAAABzEEyIUICAWNbbr1AzkKTImdU8G"></div>
-                <input type="submit" value="Join" class="btn btn-primary form-control mt-1" />
+                <input type="button" id="join" value="Join" class="btn btn-primary form-control mt-1" />
             </div>
 
         </div>
     </div>
 </form>
-
+<script src="js/newUser.js"></script>
 <%@include file="jsp/footer.jsp"%>
